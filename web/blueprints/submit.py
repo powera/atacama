ab0a82980fd600ae79edeaa145d537d52b5d69e2
Alexander Power <powera@gmail.com>
2024-12-30 13:43:46 -0600

    another import fix

--- web/blueprints/submit.py
+++ web/blueprints/submit.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, render_template, url_for, redirect, session
+from flask import Blueprint, request, render_template, url_for, redirect, session, jsonify
 from sqlalchemy.orm import joinedload
 import logging
 logger = logging.getLogger(__name__)
