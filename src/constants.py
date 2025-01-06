4786057b3cd0b38911e78f927ac5099a53daf6ef
Alexander Power <powera@gmail.com>
2025-01-06 14:50:40 -0600

    initial openai_client
    we do not use the "official" python client, because of import issues.

--- src/constants.py
+++ src/constants.py
@@ -2,7 +2,9 @@ import os
 
 # Get the src directory
 SRC_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(SRC_DIR)
 
 # Define common paths relative to project root
 WEB_DIR = os.path.join(SRC_DIR, "web")
 STATIC_DIR = os.path.join(WEB_DIR, "static")
+KEY_DIR = os.path.join(PROJECT_ROOT, "keys")
