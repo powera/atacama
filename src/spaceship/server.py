f357eb319ea5b58638b193193ea21b2656221506
Alexander Power <powera@gmail.com>
2025-03-26 15:36:30 -0500

    fix import

--- src/spaceship/server.py
+++ src/spaceship/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_file, render_template_string
+from flask import Flask, send_file, render_template_string, send_from_directory
 import subprocess
 import os
 import threading
